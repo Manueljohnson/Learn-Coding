54827b21c7cbbad05a003b8e8e3390442efb091f
Manueljohnson <72248531+Manueljohnson@users.noreply.github.com>
2020-10-02 20:55:07 +0530

    Create Manueljohnson.c

---
+++ 4.C/Manueljohnson.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : MANUEL JOHNSON\n");
+     printf("Age    : 19\n");
+     printf("Username : Manueljohnson\n");
+     return(0);
+  }
